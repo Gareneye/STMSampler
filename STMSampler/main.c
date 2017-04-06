cf6fc751b39d152085e462d35cc96bd23563427e
Maciej Olejnik <maciekolej@gmail.com>
2017-04-06 02:07:31 +0200

    Added Audio (codec) library and some magic

--- STMSampler/main.c
+++ STMSampler/main.c
@@ -1,181 +1,51 @@
-#include "stm32f4xx_conf.h"
-#include "stm32f4xx_gpio.h"
-#include "stm32f4xx_rcc.h"
-#include "stm32f4xx_dac.h"
-#include "stm32f4xx_adc.h"
-#include "stm32f4xx_tim.h"
-#include "stm32f4xx_exti.h"
-#include "stm32f4xx_syscfg.h"
-#include "misc.h"
+#include "STM32F4-Discovery/stm32f4_discovery_audio_codec.h"
+#include "audio_sample.h"
+#include "codec.h"
 
-#include "buttons.h"
+uint32_t retVal_Init;
+uint32_t retVal_Play;
+uint32_t retVal_PauseResume;
 
-uint16_t ADC_Result;
+void configureAudio(void) {
+	EVAL_AUDIO_SetAudioInterface(AUDIO_INTERFACE_I2S);
+	retVal_Init = EVAL_AUDIO_Init(OUTPUT_DEVICE_HEADPHONE, 70, 44000);
 
-const int MAX = 32000;
-float glosnosc = 1.0;
-unsigned int j = 0;
-int i=0;
-int x=1;
-extern const u8 a[32000];
-
-void TIM2_IRQHandler (void){
-	if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET){
-
-		if(GPIO_ReadInputDataBit(GPIOD,GPIO_Pin_0)==1)
-		{
-			float wartosc = ((float)a[j] * glosnosc);
-			DAC_SetChannel1Data(DAC_Align_12b_R, (int)wartosc);
-			if(++j >= MAX) j = 0;
-		}
-		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-
-	}
-}
-
-void TIM3_IRQHandler(void)
-{
-	if(TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)
-	{
-		if(i<(4))
-		{
-			GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
-		}
-		if(i>=(4) && i<(8))
-		{
-			GPIO_ToggleBits(GPIOD, GPIO_Pin_13);
-		}
-		if(i<(4))
-		{
-			GPIO_ToggleBits(GPIOD, GPIO_Pin_14);
-		}
-		if(i>=(4) && i<(8))
-		{
-			GPIO_ToggleBits(GPIOD, GPIO_Pin_15);
-		}
-		i++;
-		if(i==8)i=0;
-
-		TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
-	}
-}
-
-void configureD(void) {
-
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-
-	GPIO_InitTypeDef GPIO_InitButtonsStruct;
-	GPIO_InitButtonsStruct.GPIO_Pin = GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15;
-	GPIO_InitButtonsStruct.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitButtonsStruct.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitButtonsStruct.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitButtonsStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
-	GPIO_Init(GPIOD, &GPIO_InitButtonsStruct);
-}
-
-void configureTIM(void) {
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
-
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-
-	TIM_TimeBaseStructure.TIM_Period = 1700;
-	TIM_TimeBaseStructure.TIM_Prescaler = 0;
-	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-	TIM_TimeBaseStructure.TIM_CounterMode =  TIM_CounterMode_Up;
-	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
-
-	TIM_Cmd(TIM2, ENABLE);
-
-	TIM_TimeBaseStructure.TIM_Period = 4999;
-	TIM_TimeBaseStructure.TIM_Prescaler = 1999;
-	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-	TIM_TimeBaseStructure.TIM_CounterMode =  TIM_CounterMode_Up;
-	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
-
-	TIM_Cmd(TIM3, ENABLE);
-}
-
-void configureNVIC(void) {
-
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
-
-	NVIC_InitTypeDef NVIC_InitStructure;
-	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x00;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x00;
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&NVIC_InitStructure);
-
-	TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
-
-
-	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x00;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x00;
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&NVIC_InitStructure);
-
-	TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
-	TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
-}
-
-void configureADC(void) {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
-
-	ADC_CommonInitTypeDef ADC_CommonInitStructure;
-	ADC_CommonInitStructure.ADC_Mode = ADC_Mode_Independent;
-	ADC_CommonInitStructure.ADC_Prescaler = ADC_Prescaler_Div2;
-	ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
-	ADC_CommonInitStructure.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles;
-	ADC_CommonInit(&ADC_CommonInitStructure);
-
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 1, ADC_SampleTime_84Cycles);
-
-	ADC_Cmd(ADC1, ENABLE);
-}
-
-void configureDAC(void) {
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA , ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_DAC, ENABLE);
-
-	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_4;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-	DAC_InitTypeDef DAC_InitStructure;
-	DAC_InitStructure.DAC_Trigger = DAC_Trigger_None;
-	DAC_InitStructure.DAC_WaveGeneration = DAC_WaveGeneration_None;
-	DAC_InitStructure.DAC_LFSRUnmask_TriangleAmplitude = DAC_LFSRUnmask_Bit0;
-	DAC_InitStructure.DAC_OutputBuffer = DAC_OutputBuffer_Enable;
-	DAC_Init(DAC_Channel_1, &DAC_InitStructure);
-
-	DAC_Cmd(DAC_Channel_1, ENABLE);
+	retVal_Play = EVAL_AUDIO_Play((uint16_t*)(AUDIO_SAMPLE + 58), 900000);
 }
 
 int main(void)
 {
 	SystemInit();
 	
-	//configureButtons();
-	configureD();
-	configureTIM();
-	configureNVIC();
-	configureADC();
-	configureDAC();
+	/* Magic! */
+	codec_init();
+	codec_ctrl_init();
+	configureAudio();
 
-	for(;;) {
-		ADC_SoftwareStartConv(ADC1);
-		while(ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == RESET);
-		glosnosc = (float)ADC_GetConversionValue(ADC1)/4095.0;
-	}
+	while(1) {}
+}
+
+uint16_t EVAL_AUDIO_GetSampleCallBack(void)
+{
+	return 1;
+}
+
+void EVAL_AUDIO_TransferComplete_CallBack(uint32_t pBuffer, uint32_t Size)
+{
+	retVal_Play = EVAL_AUDIO_Play((uint16_t*)(AUDIO_SAMPLE + 58), 900000);
+}
+
+void EVAL_AUDIO_HalfTransfer_CallBack(uint32_t pBuffer, uint32_t Size)
+{
+}
+
+void EVAL_AUDIO_Error_CallBack(void* pData)
+{
+	while(1);
+}
+
+uint32_t Codec_TIMEOUT_UserCallback(void)
+{
+	while(1);
+	return 1;
 }
